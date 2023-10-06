fcb6eae91f72891d35bc1f153f846423a4701d90
Zeke Piskulich <piskulichz@gmail.com>
2023-10-06 12:38:24 -0400

    parentheses

--- field_module.f90
+++ field_module.f90
@@ -5,7 +5,7 @@ MODULE field_module
     REAL FUNCTION E_Cont(q, r1, r2, dist) RESULT(contribution)
         IMPLICIT NONE
         REAL :: q, r1, r2, dist
-        contribution = q * (r1 - r2) / dist**3
+        contribution = q * (r1 - r2) / (dist**3)
     END FUNCTION E_Cont
 
     include 'read_field_input.f90'
