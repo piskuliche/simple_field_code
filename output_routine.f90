cc98de38f5f3efd7ddfea98d02ac0bc78d741c4a
Zeke Piskulich <piskulichz@gmail.com>
2023-10-10 14:51:27 -0400

    fixed dims

--- output_routine.f90
+++ output_routine.f90
@@ -45,7 +45,7 @@ DO i=1, nmol
     CALL h5screate_simple(1, [3, nconfig], dataspace_id, ERROR_FLAG)
     WRITE(dataset_name, '(A, I0)') "eoh_", (i-1)*2+1
     CALL h5dcreate_f(file_id, trim(dataset_name), H5T_NATIVE_REAL, dataspace_id, dataset_id, ERROR_FLAG)
-    CALL h5dwrite_f(dataset_id, H5T_NATIVE_REAL, eoh1(i,:,:),[3,nconfig], ERROR_FLAG)
+    CALL h5dwrite_f(dataset_id, H5T_NATIVE_REAL, eoh1(i,:,:), eoh_dims, ERROR_FLAG)
 
     CALL h5dclose_f(dataset_id, ERROR_FLAG)
     CALL h5sclose_f(dataspace_id, ERROR_FLAG)
@@ -55,7 +55,7 @@ DO i=1, nmol
     CALL h5screate_simple(1, [nconfig], dataspace_id, ERROR_FLAG)
     WRITE(dataset_name, '(A, I0)') "dot_", (i-1)*2+2
     CALL h5dcreate_f(file_id, trim(dataset_name), H5T_NATIVE_REAL, dataspace_id, dataset_id, ERROR_FLAG)
-    CALL h5dwrite_f(dataset_id, H5T_NATIVE_REAL, dot2(i,:), [nconfig], ERROR_FLAG)
+    CALL h5dwrite_f(dataset_id, H5T_NATIVE_REAL, dot2(i,:), dot_dims, ERROR_FLAG)
 
     CALL h5dclose_f(dataset_id, ERROR_FLAG)
     CALL h5sclose_f(dataspace_id, ERROR_FLAG)
@@ -63,7 +63,7 @@ DO i=1, nmol
     CALL h5screate_simple(1, [3, nconfig], dataspace_id, ERROR_FLAG)
     WRITE(dataset_name, '(A, I0)') "eoh_", (i-1)*2+1
     CALL h5dcreate_f(file_id, trim(dataset_name), H5T_NATIVE_REAL, dataspace_id, dataset_id, ERROR_FLAG)
-    CALL h5dwrite_f(dataset_id, H5T_NATIVE_REAL, eoh2(i,:,:), [3,nconfig], ERROR_FLAG)
+    CALL h5dwrite_f(dataset_id, H5T_NATIVE_REAL, eoh2(i,:,:), eoh_dims, ERROR_FLAG)
 
     CALL h5dclose_f(dataset_id, ERROR_FLAG)
     CALL h5sclose_f(dataspace_id, ERROR_FLAG)
